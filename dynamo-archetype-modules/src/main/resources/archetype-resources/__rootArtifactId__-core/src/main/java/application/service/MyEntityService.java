eed88049070cb409a6d9096104714a2b7a10e64d
Endy Brosens <endy.brosens@opencirclesolutions.nl>
2016-06-24 13:44:57 +0200

    `Much work there is, creating a Multi-Module Maven Project`

---
+++ dynamo-archetype-modules/src/main/resources/archetype-resources/__rootArtifactId__-core/src/main/java/application/service/MyEntityService.java
@@ -0,0 +1,8 @@
+package ${package}.application.service;
+
+import com.ocs.dynamo.service.BaseService;
+
+import ${package}.application.domain.MyEntity;
+
+public interface MyEntityService extends BaseService<Integer, MyEntity> {
+}
