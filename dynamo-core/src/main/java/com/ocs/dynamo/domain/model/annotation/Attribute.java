8966774a9202a328b96b1625ca64857ff72548e1
willemvd <willem@opencirclesolutions.nl>
2016-07-08 10:48:09 +0200

    use javadoc instead of comments, will now show up in editor

--- dynamo-core/src/main/java/com/ocs/dynamo/domain/model/annotation/Attribute.java
+++ dynamo-core/src/main/java/com/ocs/dynamo/domain/model/annotation/Attribute.java
@@ -13,15 +13,15 @@
  */
 package com.ocs.dynamo.domain.model.annotation;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
 import com.ocs.dynamo.domain.model.AttributeDateType;
 import com.ocs.dynamo.domain.model.AttributeSelectMode;
 import com.ocs.dynamo.domain.model.AttributeTextFieldMode;
 import com.ocs.dynamo.domain.model.VisibilityType;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * An interface that can be used to specify the properties of an attribute - this will override any
@@ -33,118 +33,117 @@ import com.ocs.dynamo.domain.model.VisibilityType;
 @Target({ ElementType.FIELD, ElementType.METHOD })
 public @interface Attribute {
 
-    // the allowed extensions in case a file upload is used to edit the
-    // attribute
+    /** the allowed extensions in case a file upload is used to edit the attribute */
     String allowedExtensions() default "";
 
-    // whether a complex attribute is directly editable
+    /** whether a complex attribute is directly editable */
     boolean complexEditable() default false;
 
-    // is this a currency field?
+    /** is this a currency field? */
     boolean currency() default false;
 
-    // date type
+    /** date type */
     AttributeDateType dateType() default AttributeDateType.INHERIT;
 
-    // the default value (represented as a string - use "." as the decimal
-    // separator)
+    /** the default value (represented as a string - use "." as the decimal separator) */
     String defaultValue() default "";
 
-    // textual description
+    /** textual description */
     String description() default "";
 
-    // should the field get focus in a detail table
+    /** should the field get focus in a detail table */
     boolean detailFocus() default false;
 
-    // the display format (useful in case of dates)
+    /** the display format (useful in case of dates) */
     String displayFormat() default "";
 
-    // the display name
+    /** the display name */
     String displayName() default "";
 
-    // is this an embedded object
+    /** is this an embedded object */
     boolean embedded() default false;
 
-    // the representation to use instead of "false"
+    /** the representation to use instead of "false" */
     String falseRepresentation() default "";
 
-    // the name of the property in which to store the file name (after an upload)
+    /** the name of the property in which to store the file name (after an upload) */
     String fileNameProperty() default "";
 
-    // whether the component should be represented as an image
+    /** whether the component should be represented as an image */
     boolean image() default false;
 
-    // whether this field is the main attribute
+    /** whether this field is the main attribute */
     boolean main() default false;
 
-    // whether this attribute allows search for multiple values (only for MASTER attributes)
+    /** whether this attribute allows search for multiple values (only for MASTER attributes) */
     boolean multipleSearch() default false;
 
-    // the maximum element length (-1 indicates no value)
+    /** the maximum element length (-1 indicates no value) */
     int maxLength() default -1;
 
-    // the type of the members in a detail collection - needed in very specific cases
+    /** the type of the members in a detail collection - needed in very specific cases */
     Class<?> memberType() default Object.class;
 
-    // the minimum element length (-1 indicates no value)
+    /** the minimum element length (-1 indicates no value) */
     int minLength() default -1;
 
-    // is the numeric field a percentage
+    /** is the numeric field a percentage */
     boolean percentage() default false;
 
-    // decimal precision
+    /** decimal precision */
     int precision() default -1;
 
-    // prompt value to use in input fields
+    /** prompt value to use in input fields */
     String prompt() default "";
 
+    /** */
     String quickAddPropertyName() default "";
 
-    // whether the field is readonly (i.e. does not appear in edit forms)
+    /** whether the field is readonly (i.e. does not appear in edit forms) */
     boolean readOnly() default false;
 
-    //
+    /** */
     boolean required() default false;
 
-    // replacement search path
+    /** replacement search path */
     String replacementSearchPath() default "";
 
-    // whether the field is searchable
+    /** whether the field is searchable */
     boolean searchable() default false;
 
-    // case sensitive search
+    /** case sensitive search */
     boolean searchCaseSensitive() default false;
 
-    // search prefix only
+    /** search prefix only */
     boolean searchPrefixOnly() default false;
 
-    // whether to search by single value (for integer or date fields)
+    /** whether to search by single value (for integer or date fields) */
     boolean searchForExactValue() default false;
 
-    // selection mode (use either a combo box, a list box, or a lookup field)
+    /** selection mode (use either a combo box, a list box, or a lookup field) */
     AttributeSelectMode selectMode() default AttributeSelectMode.INHERIT;
 
-    // whether the field shows up in a table
+    /** whether the field shows up in a table */
     VisibilityType showInTable() default VisibilityType.INHERIT;
 
-    // whether a table can be sorted on this field
+    /** whether a table can be sorted on this field */
     boolean sortable() default true;
 
-    // whether to display a text attribute as a text field or a text area
+    /** whether to display a text attribute as a text field or a text area */
     AttributeTextFieldMode textFieldMode() default AttributeTextFieldMode.INHERIT;
 
-    // the representation to use instead of "true"
+    /** the representation to use instead of "true" */
     String trueRepresentation() default "";
 
-    // indicates whether this is a clickable URL
+    /** indicates whether this is a clickable URL */
     boolean url() default false;
 
-    // indicates whether to use thousands grouping for an integer or decimal field
+    /** indicates whether to use thousands grouping for an integer or decimal field */
     boolean useThousandsGrouping() default true;
 
-    // whether the field is visible
+    /** whether the field is visible */
     VisibilityType visible() default VisibilityType.INHERIT;
 
-    // whether the field represents a date that must be shown in week notation
+    /** whether the field represents a date that must be shown in week notation */
     boolean week() default false;
 }
