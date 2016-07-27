036e21d0c16d1122fde36c664fe53a36af64e9d8
willemvd <willem@opencirclesolutions.nl>
2016-07-27 10:43:41 +0200

    * handle all values so the can be used to search * some initial styling

--- dynamo-spring/src/main/java/com/ocs/dynamo/ui/component/TokenFieldSelect.java
+++ dynamo-spring/src/main/java/com/ocs/dynamo/ui/component/TokenFieldSelect.java
@@ -2,69 +2,104 @@ package com.ocs.dynamo.ui.component;
 
 import com.explicatis.ext_token_field.ExtTokenField;
 import com.explicatis.ext_token_field.SimpleTokenizable;
+import com.explicatis.ext_token_field.Tokenizable;
 import com.ocs.dynamo.domain.AbstractEntity;
 import com.ocs.dynamo.domain.model.AttributeModel;
 import com.ocs.dynamo.domain.model.EntityModel;
 import com.ocs.dynamo.service.BaseService;
 import com.ocs.dynamo.utils.ClassUtils;
+import com.vaadin.annotations.StyleSheet;
 import com.vaadin.data.Container;
 import com.vaadin.data.Property;
 import com.vaadin.data.sort.SortOrder;
-import com.vaadin.ui.ComboBox;
+import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.ui.Component;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.HorizontalLayout;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
+@StyleSheet("tokenField.css")
 public class TokenFieldSelect<ID extends Serializable, T extends AbstractEntity<ID>> extends
-		CustomEntityField<ID, T, Object> {
+		CustomEntityField<ID, T, Collection> {
 	private final ExtTokenField extTokenField;
 	private final EntityComboBox comboBox;
+	private final BeanItemContainer<T> container;
+	private final Collection<ValueChangeListener> valueChangeListeners;
 
 	public TokenFieldSelect(EntityModel<T> em, AttributeModel attributeModel, BaseService<ID, T> service,
 							Container.Filter filter, SortOrder... sortOrders) {
 		super(service, em, attributeModel);
 		extTokenField = new ExtTokenField();
-
-		comboBox = new EntityComboBox<ID, T>(em ,attributeModel, service, sortOrders);
+		comboBox = new EntityComboBox<>(em, attributeModel, service, filter, sortOrders);
+		container = new BeanItemContainer<>(AbstractEntity.class);
+		valueChangeListeners = new ArrayList<>();
 	}
-
 
 	@Override
 	protected Component initContent() {
-		VerticalLayout layout = new DefaultVerticalLayout(false, false);
+		HorizontalLayout layout = new DefaultHorizontalLayout(false, false, false);
 
-		comboBox.setItemCaptionPropertyId("name");
 		comboBox.setInputPrompt(getMessageService().getMessage("ocs.type.to.add.new.value"));
 		extTokenField.setInputField(comboBox);
 		extTokenField.setEnableDefaultDeleteTokenAction(true);
-		comboBox.addValueChangeListener(getComboBoxValueChangeListener(extTokenField, comboBox));
+		comboBox.addValueChangeListener(getComboBoxValueChangeListener(extTokenField, comboBox, getEntityModel(), getAttributeModel()));
 
+		extTokenField.setSizeFull();
 		layout.addComponent(extTokenField);
 		return layout;
 	}
 
-	@Override
-	public Class<? extends Object> getType() {
-		return Object.class;
-	}
-
-	private ValueChangeListener getComboBoxValueChangeListener(final ExtTokenField extTokenField, final ComboBox comboBox) {
+	private ValueChangeListener getComboBoxValueChangeListener(final ExtTokenField extTokenField, final EntityComboBox comboBox, EntityModel<T> entityModel, AttributeModel attributeModel) {
 		return new ValueChangeListener() {
 			@Override
 			public void valueChange(Property.ValueChangeEvent event) {
+				Object selectedObject = event.getProperty().getValue();
+				if (selectedObject != null) {
+					T abstractEntity = (T) selectedObject;
+					container.addBean(abstractEntity);
 
-				Object countryObject = event.getProperty().getValue();
-				if (countryObject != null) {
-					SimpleTokenizable t = new SimpleTokenizable((Integer) ClassUtils.getFieldValue(countryObject, "id"), ClassUtils.getFieldValueAsString(countryObject, "name"));
-					extTokenField.addTokenizable(t);
+					Tokenizable token = new SimpleTokenizable(getTokenIdentifier(abstractEntity), getTokenDisplayName(abstractEntity, (String) comboBox.getItemCaptionPropertyId()));
+					extTokenField.addTokenizable(token);
 
-					// if you would use a real container, you would filter the selected tokens out
+					for (ValueChangeListener valueChangeListener : TokenFieldSelect.this.valueChangeListeners) {
+						valueChangeListener.valueChange(new ValueChangeEvent(TokenFieldSelect.this));
+					}
 
 					// reset combobox
 					comboBox.setValue(null);
 				}
 			}
 		};
+	}
+
+	@Override
+	public List<T> getValue() {
+		return container.getItemIds();
+	}
+
+	@Override
+	protected List<T> getInternalValue() {
+		return container.getItemIds();
+	}
+
+	@Override
+	public Class<? extends Collection> getType() {
+		return List.class;
+	}
+
+	private String getTokenDisplayName(AbstractEntity entity, String captionPropertyId) {
+		return ClassUtils.getFieldValueAsString(entity, captionPropertyId);
+	}
+
+	private long getTokenIdentifier(AbstractEntity entity) {
+		return Long.parseLong(ClassUtils.getFieldValueAsString(entity, "id"));
+	}
+
+	@Override
+	public void addValueChangeListener(final ValueChangeListener listener) {
+		valueChangeListeners.add(listener);
 	}
 }
