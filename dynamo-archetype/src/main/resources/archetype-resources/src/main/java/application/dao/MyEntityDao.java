aac98155ae6bf38c1452fe13ec8d3e0348b4bb94
OpenCircleEndy <endy.brosens@opencirclesolutions.com>
2018-02-08 21:52:05 +0100

    Don't publish sources.

--- dynamo-archetype/src/main/resources/archetype-resources/src/main/java/application/dao/MyEntityDao.java
+++
@@ -1,7 +0,0 @@
-package ${package}.application.dao;
-
-import com.ocs.dynamo.dao.BaseDao;
-import ${groupId}.application.domain.MyEntity;
-
-public interface MyEntityDao extends BaseDao<Integer, MyEntity> {
-}
