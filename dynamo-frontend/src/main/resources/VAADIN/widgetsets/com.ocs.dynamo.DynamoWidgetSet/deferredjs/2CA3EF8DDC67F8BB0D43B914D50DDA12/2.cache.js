d7d72adbcb3161ab606b4090a8576a7535afd4cd
wittekip <bas.rutten@opencirclesolutions.nl>
2018-02-23 12:01:06 +0100

    added switch component

--- dynamo-frontend/src/main/resources/VAADIN/widgetsets/com.ocs.dynamo.DynamoWidgetSet/deferredjs/2CA3EF8DDC67F8BB0D43B914D50DDA12/2.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.com_ocs_dynamo_DynamoWidgetSet.runAsyncCallback2('Zjb(1651,1,Jae);_.ce=function Gmc(){A6b((!t6b&&(t6b=new F6b),t6b),this.a.d)};C3d(Jm)(2);\n//# sourceURL=com.ocs.dynamo.DynamoWidgetSet-2.js\n')
