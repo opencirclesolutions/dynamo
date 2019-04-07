693b97c91ab97de10610a6a8f04bb215afe9f6c6
wittekip <bas.rutten@opencirclesolutions.nl>
2019-04-07 11:16:49 +0200

    removed gantt

--- dynamo-frontend-export/src/main/resources/VAADIN/widgetsets/com.ocs.dynamo.DynamoWidgetSet/deferredjs/286C48E441BB4EFDC56AF1F8A4477708/2.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.com_ocs_dynamo_DynamoWidgetSet.runAsyncCallback2('wsb(1974,1,VDe);_.fe=function MGc(){Bic((!tic&&(tic=new Jic),tic),this.a.d)};Swe(Wm)(2);\n//# sourceURL=com.ocs.dynamo.DynamoWidgetSet-2.js\n')
