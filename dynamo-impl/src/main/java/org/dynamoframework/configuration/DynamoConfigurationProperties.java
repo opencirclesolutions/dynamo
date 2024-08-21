fa800d1ede86661341b9b8aaf66a79778fb2452c
Tommy Menheere <tommy.menheere@opencirclesolutions.nl>
2024-08-21 16:05:10 +0200

    Updated license + file headers, see #232

--- dynamo-impl/src/main/java/org/dynamoframework/configuration/DynamoConfigurationProperties.java
+++ dynamo-impl/src/main/java/org/dynamoframework/configuration/DynamoConfigurationProperties.java
@@ -1,5 +1,25 @@
 package org.dynamoframework.configuration;
 
+/*-
+ * #%L
+ * Dynamo Framework
+ * %%
+ * Copyright (C) 2014 - 2024 Open Circle Solutions
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import lombok.Data;
 import org.dynamoframework.domain.model.*;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -17,10 +37,6 @@ import java.util.Map;
 public class DynamoConfigurationProperties implements Serializable, DynamoProperties {
     @Serial
     private static final long serialVersionUID = -8329492319537273489L;
-
-    /**
-     * Indicates whether to capitalize individual words in property names
-     */
     private boolean capitalizePropertyNames = true;
 
     /**
--- dynamo-impl/src/main/java/org/dynamoframework/domain/model/impl/EntityModelActionImpl.java
+++ dynamo-impl/src/main/java/org/dynamoframework/domain/model/impl/EntityModelActionImpl.java
@@ -1,5 +1,25 @@
 package org.dynamoframework.domain.model.impl;
 
+/*-
+ * #%L
+ * Dynamo Framework
+ * %%
+ * Copyright (C) 2014 - 2024 Open Circle Solutions
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -42,16 +62,6 @@ public class EntityModelActionImpl implements EntityModelAction {
     public String getDisplayName(Locale locale) {
         return lookup(displayNames, locale, EntityModel.DISPLAY_NAME, defaultDisplayName);
     }
-
-    /**
-     * Looks up a text message from a resource bundle
-     *
-     * @param source   the message cache
-     * @param locale   the desired locale
-     * @param key      the message key
-     * @param fallBack value to fall back to if no match is found
-     * @return the translation of the key
-     */
     private String lookup(Map<String, Optional<String>> source, Locale locale, String key, String fallBack) {
         // look up in message bundle and add to cache
         if (!source.containsKey(locale.toString())) {
