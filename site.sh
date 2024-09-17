908fe18fd5dadac7929c3cf1915272aaeb480f3c
Tommy Menheere <tommy.menheere@opencirclesolutions.nl>
2024-09-17 17:11:13 +0200

    Updates

--- site.sh
+++ site.sh
@@ -5,11 +5,15 @@
 
 export NVP_API_KEY=$(op read "op://sxetjycambbujpjbtcztimp7xa/NVP API Key/credential")
 
-export DEPENDENCY_SKIP="true"
+export TEST_SKIP="false"
+export DEPENDENCY_SKIP="false"
+export PUBLISH_DRYRUN="false"
 
 mvn -Ddependency.skip="${DEPENDENCY_SKIP}" \
     -DconnectionString="jdbc:postgresql://$(op read "op://sxetjycambbujpjbtcztimp7xa/CVE database/server"):$(op read "op://sxetjycambbujpjbtcztimp7xa/CVE database/port")/$(op read "op://sxetjycambbujpjbtcztimp7xa/CVE database/database")?sslmode=require" \
     -DdatabaseUser="$(op read "op://sxetjycambbujpjbtcztimp7xa/CVE database/username")" \
     -DdatabasePassword="$(op read "op://sxetjycambbujpjbtcztimp7xa/CVE database/password")" \
     -DdatabaseDriverName="org.postgresql.Driver" \
-    clean site site:stage
+    -Dscmpublish.dryRun="${PUBLISH_DRYRUN}" \
+    -DskipTests="${TEST_SKIP}" \
+    clean verify site site:stage scm-publish:publish-scm
