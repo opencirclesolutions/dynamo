0a8a52f1def329c438791b90ed93d8b2b2b64db7
Tommy Menheere <tommy.menheere@opencirclesolutions.nl>
2024-09-18 13:14:05 +0200

    Moved all documentation to src/docs/asciidoc in parent

--- site.sh
+++ site.sh
@@ -5,15 +5,15 @@
 
 export NVP_API_KEY=$(op read "op://sxetjycambbujpjbtcztimp7xa/NVP API Key/credential")
 
-export TEST_SKIP="false"
-export DEPENDENCY_SKIP="false"
-export PUBLISH_DRYRUN="false"
+export SKIP_TESTS="true"
+export SKIP_DEPENDENCY_CHECK="true"
+export DRY_RUN_PUBLISH="true"
 
-mvn -Ddependency.skip="${DEPENDENCY_SKIP}" \
+mvn -Ddependency.skip="${SKIP_DEPENDENCY_CHECK}" \
     -DconnectionString="jdbc:postgresql://$(op read "op://sxetjycambbujpjbtcztimp7xa/CVE database/server"):$(op read "op://sxetjycambbujpjbtcztimp7xa/CVE database/port")/$(op read "op://sxetjycambbujpjbtcztimp7xa/CVE database/database")?sslmode=require" \
     -DdatabaseUser="$(op read "op://sxetjycambbujpjbtcztimp7xa/CVE database/username")" \
     -DdatabasePassword="$(op read "op://sxetjycambbujpjbtcztimp7xa/CVE database/password")" \
     -DdatabaseDriverName="org.postgresql.Driver" \
-    -Dscmpublish.dryRun="${PUBLISH_DRYRUN}" \
-    -DskipTests="${TEST_SKIP}" \
+    -Dscmpublish.dryRun="${DRY_RUN_PUBLISH}" \
+    -DskipTests="${SKIP_TESTS}" \
     clean verify site site:stage scm-publish:publish-scm
