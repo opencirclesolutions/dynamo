e840ebc446c1edb04e18adf9d4b429a1c21b066e
Bas Rutten <bas.rutten@edsn.nl>
2025-02-21 13:21:46 +0100

    Removed some tests

--- dynamo-angular/src/main/dynamo/projects/dynamo-angular/src/lib/directives/additional-validation.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AdditionalValidationDirective } from './additional-validation.directive';
-
-describe('AdditionalValidationDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AdditionalValidationDirective();
-    expect(directive).toBeTruthy();
-  });
-});
